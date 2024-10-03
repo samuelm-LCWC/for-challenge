6b35ee0dfe3ada350fddd03d8a12e75e68789d64
samuelm-LCWC <samuelm@lcwc.ac.uk>
2024-10-03 15:31:27 +0100

    updated test file

--- test_task.py
+++ test_task.py
@@ -6,17 +6,34 @@ def test_task_1_length():
     result = task_1()
     assert len(result) == 6
 
-@pytest.mark.parametrize("n", range(10))
-def test_task_1_values(n):
-    result = task_1()
-    for i in result:
-        assert 1 <= i <= 49
+def test_task_1_values():
+    for test in range(5):
+        result = task_1()
+        for i in result:
+            assert 1 <= i <= 49
 
-@pytest.mark.parametrize("endpoint", [90, 80, 70, 60])
-def test_task_2(endpoint):
-    with patch("builtins.input", return_value=(str(endpoint))):
+def test_task_2_case_90():
+    with patch("builtins.input", return_value="90"):
         result = task_2()
-        check_list = [i for i in range(100, endpoint - 1, -1)]
+        check_list = [i for i in range(100, 89, -1)]
+        assert result == check_list
+
+def test_task_2_case_80():
+    with patch("builtins.input", return_value="80"):
+        result = task_2()
+        check_list = [i for i in range(100, 79, -1)]
+        assert result == check_list
+
+def test_task_2_case_70():
+    with patch("builtins.input", return_value="70"):
+        result = task_2()
+        check_list = [i for i in range(100, 69, -1)]
+        assert result == check_list
+
+def test_task_2_case_60():
+    with patch("builtins.input", return_value="60"):
+        result = task_2()
+        check_list = [i for i in range(100, 59, -1)]
         assert result == check_list
 
 def test_task_3():
